6bfce342aba0e915843cec5c48d17c8ac50cde2e
Amir Kamel <amirkamel@geniesolutions.com.au>
2020-07-02 13:39:09 +1000

    Updated installation requirments

--- pip-install.sh
+++ pip-install.sh
@@ -12,6 +12,9 @@ source venv/bin/activate
 echo "Install requirements for project."
 pip install -r requirements.txt
 
+echo "Install XGBOOST package"
+conda install -c anaconda py-xgboost
+
 echo "Add venv kernel to jupyter."
 python -m ipykernel install --user --name=venv --display-name="Python (COVID19_RandomForest)"
 
